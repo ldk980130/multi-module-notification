0f169fafeac95eef13220111d0254e759c85207f
DOES <ldk980130@gmail.com>
2022-11-24 11:32:42 +0900

    Initial commit

---
+++ src/main/java/com/practice/multimodulenotification/MultiModuleNotificationApplication.java
@@ -0,0 +1,12 @@
+package com.practice.multimodulenotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModuleNotificationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/practice/multimodulenotification/MultiModuleNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.multimodulenotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
